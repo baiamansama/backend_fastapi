e082e30e6629ce4f1057b50a4ebd840d2d715924
Baiaman <baiamansama@gmail.com>
2023-11-25 15:39:10 +0900

    fixed redis integration

--- auth/auth.py
+++ auth/auth.py
@@ -1,15 +1,29 @@
 from fastapi_users.authentication import CookieTransport, AuthenticationBackend
-from fastapi_users.authentication import JWTStrategy
+from fastapi_users.authentication import JWTStrategy, RedisStrategy
+import redis.asyncio
+
+redis = redis.asyncio.from_url("redis://localhost:6379", decode_responses=True)
 
 cookie_transport = CookieTransport(cookie_name="elice", cookie_max_age=3600)
 
 SECRET = "SECRET"
 
-def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+# def get_jwt_strategy() -> JWTStrategy:
+#     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+# jwt_auth_backend = AuthenticationBackend(
+#     name="jwt",
+#     transport=cookie_transport,
+#     get_strategy=get_jwt_strategy,
+# )
+
+
+def get_redis_strategy() -> RedisStrategy:
+    return RedisStrategy(redis, lifetime_seconds=3600)
 
 auth_backend = AuthenticationBackend(
-    name="jwt",
+    name="redis",
     transport=cookie_transport,
-    get_strategy=get_jwt_strategy,
+    get_strategy=get_redis_strategy,
 )
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from pydantic import BaseModel, Field
 
 from fastapi import FastAPI, Request, status, Depends
 from fastapi.encoders import jsonable_encoder
-# from fastapi.exceptions import ValidationError
 from fastapi.responses import JSONResponse
 
 from auth.auth import auth_backend
@@ -17,10 +16,6 @@ from auth.schemas import UserRead, UserCreate
 from board.board_api import router as board_router
 from post.post_api import router as post_router
 
-from fastapi_cache.backends.redis import RedisBackend
-from fastapi_cache.decorator import cache
-
-from redis import asyncio as aioredis
 
 app = FastAPI(
     title="mini project"
@@ -33,7 +28,7 @@ fastapi_users = FastAPIUsers[User, int](
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
+    prefix="/auth/redis",
     tags=["auth"],
 )
 
@@ -47,8 +42,3 @@ current_user = fastapi_users.current_user()
 
 app.include_router(board_router)
 app.include_router(post_router)
-
-@app.on_event("startup")
-async def startup():
-    redis = aioredis.from_url("redis://localhost")
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
